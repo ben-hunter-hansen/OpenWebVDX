9ea9aae06eb29aab5a8c00ddfe4fd47c6f9b9bde
ben-hunter-hansen <ben.hunter.hansen@gmail.com>
2014-11-27 20:54:41 -0600

    Async HTML5 drag/drop file uploads added

---
+++ OpenWebVDX/API/FileHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OpenWebVDX.API.FileHandler
+{
+   public class VDXFile
+   {
+       private HttpPostedFileBase file;
+
+       public VDXFile(HttpPostedFileBase file)
+       {
+           this.file = file;
+       }
+
+       public void writeToAppData(HttpContextBase context)
+       {
+           int fileSize = file.ContentLength;
+           string fileName = file.FileName;
+           string mimeType = file.ContentType;
+           System.IO.Stream fileContent = file.InputStream;
+           file.SaveAs(context.Server.MapPath("~/App_Data/uploads/") + fileName); //File will be saved in application root
+       }
+   }
+}
