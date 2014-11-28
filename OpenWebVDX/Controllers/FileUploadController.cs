433fb6691448c3b2180403a4e1702996326bd2be
b_hanz_89 <ben.hunter.hansen@gmail.com>
2014-11-27 20:49:35 -0600

    Async HTML drag/drop file uploads added

---
+++ OpenWebVDX/Controllers/FileUploadController.cs
@@ -0,0 +1,35 @@
+﻿using OpenWebVDX.API.FileHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenWebVDX.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        //
+        // GET: /FileUpload/
+
+        [HttpGet]
+        [ActionName("Upload")]
+        public ActionResult GetUploadView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("UploadRequest")]
+        public JsonResult UploadFile()
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                HttpPostedFileBase file = Request.Files[i]; //Uploaded file
+                VDXFile vdxFile = new VDXFile(file);
+                vdxFile.writeToAppData(this.HttpContext);
+            }
+            return Json("Uploaded " + Request.Files.Count + " files");
+        }
+    }
+}
